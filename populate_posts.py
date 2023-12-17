6f08e218f8d57aeef7dcbfea7c8e3e613b611091
Tetsui Ohkubo <peryaudo@gmail.com>
2023-12-16 20:56:58 -0800

    Fix a bug in populate_posts.py that fewshot examples are unused

--- populate_posts.py
+++ populate_posts.py
@@ -3,7 +3,10 @@ import sqlite3
 from tqdm import tqdm
 from transformers import pipeline
 import torch
+
+print("Loading model...")
 pipe = pipeline(task="text-generation", model="rinna/youri-7b-chat", device=0, torch_dtype=torch.float16)
+print("Done.")
 
 fewshot_examples = [tuple(uttr.split(': ')) for uttr in open('fewshot_examples.txt').readlines()]
 
@@ -11,15 +14,15 @@ def generate(topic):
     instruction = "匿名掲示板5ちゃんねるの投稿者として、以下のニュースにコメントしてください。"
     input = topic
     
-    context = [
-        {
-            "speaker": "設定",
-            "text": instruction
-        },
-        {
-            "speaker": "ユーザー",
-            "text": input
-        }
+    context = [{
+        "speaker": "設定",
+        "text": instruction
+    }]
+    context += fewshot_examples
+    context += [{
+        "speaker": "ユーザー",
+        "text": input
+    }
     ]
     prompt = [
         f"{uttr['speaker']}: {uttr['text']}"
@@ -37,7 +40,7 @@ def generate(topic):
 
 conn = sqlite3.connect('database.db')
 
-for thread_id, title in tqdm(conn.execute("SELECT id, topic FROM thread").fetchall()):
+for thread_id, title in tqdm(conn.execute("SELECT id, title FROM thread").fetchall()):
     for _ in range(random.randint(1, 5)):
         generated_text = generate(title)
         conn.execute("INSERT INTO post (thread_id, body) VALUES (?, ?)", (thread_id,generated_text))
