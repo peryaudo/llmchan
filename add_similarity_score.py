1039ef1f4f8028a21f7446972d4fc81482f8b76c
Tetsui Ohkubo <peryaudo@gmail.com>
2024-01-01 18:43:35 -0800

    Filter out outliers along with calculating similarity

--- add_similarity_score.py
+++ add_similarity_score.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer, util
 from datasets import load_from_disk
 import torch.nn.functional as F
+import numpy as np
 
 model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
 
@@ -11,4 +12,7 @@ def add_similarity_score(example):
 
 dataset = load_from_disk("converted")
 dataset = dataset.map(add_similarity_score, batched=True, batch_size=1000, num_proc=1)
+lb = np.percentile(dataset['train']['similarity'], 50)
+ub = np.percentile(dataset['train']['similarity'], 95)
+dataset = dataset.filter(lambda example: (example['similarity'] >= lb) and (example['similarity'] <= ub))
 dataset.save_to_disk("converted_with_score")
